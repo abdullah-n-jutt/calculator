e91cba1e596981907a66c97983c3329f07fdb5a1
Abdullah Nasir <abdullah.n.5888@gmail.com>
2024-06-04 08:13:35 +0000

    Tue, Jun 4, 2024, 1:13 PM +05:00

---
+++ Problem 0/einstein.py
@@ -0,0 +1,15 @@
+def calculate_energy(mass):
+
+    speed_of_light = 300000000
+
+    energy = mass * speed_of_light ** 2
+    return energy
+
+def main():
+    mass = int(input("Enter mass (in kilograms): "))
+
+    energy = calculate_energy(mass)
+
+    print("Equivalent energy:", energy, "Joules")
+
+    main()
