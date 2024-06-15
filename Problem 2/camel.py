4bbb545680a1270e8f4aad092ea879338239c7dd
Abdullah Nasir <abdullah.n.5888@gmail.com>
2024-06-15 03:28:02 +0000

    Sat, Jun 15, 2024, 8:28 AM +05:00

---
+++ Problem 2/camel.py
@@ -0,0 +1,17 @@
+def camel_to_snake(name):
+    snake_case = ""
+    for c in name:
+        if c.isupper():
+            snake_case += "_" + c.lower()
+        else:
+            snake_case += c
+    return snake_case
+
+# Prompt the user for a camel case variable name
+camel_case_name = input("Enter a camel case variable name: ")
+
+# Convert to snake case
+snake_case_name = camel_to_snake(camel_case_name)
+
+# Output the snake case variable name
+print("The snake case variable name is:", snake_case_name)
